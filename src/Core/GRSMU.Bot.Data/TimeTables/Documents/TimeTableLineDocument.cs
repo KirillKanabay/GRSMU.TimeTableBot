35331a8eee86e4a90cf81ec7a58c64742ceeff0f
KirillKanabay <kirill.kanabay@gmail.com>
2024-12-01 13:45:43 +0300

    Drop existing timetables. Added TimeFrom, TimeTo to lines document

--- src/Core/GRSMU.Bot.Data/TimeTables/Documents/TimeTableLineDocument.cs
+++ src/Core/GRSMU.Bot.Data/TimeTables/Documents/TimeTableLineDocument.cs
@@ -2,7 +2,9 @@
 {
     public class TimeTableLineDocument
     {
-        public string Time { get; set; }
+        public string TimeFrom { get; set; }
+
+        public string TimeTo { get; set; }
 
         public string Subgroup { get; set; }
 
